9f1720a65ffdd62a85cfb8c90578e2562a1a8807
Carlos Ribeiro <dev.carlosribeiro@gmail.com>
2022-03-03 05:54:50 -0300

    Criação Entidades

---
+++ AlternativaSistemas.TesteTecnico/AlternativaSistemas.TesteTecnico.API/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlternativaSistemas.TesteTecnico.API.Models {
+    public class Produto {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Value { get; set; }
+        public string Brand { get; set; }
+        public int CategoryId { get; set; }
+        public Categoria Category { get; set; }
+    }
+}
---
+++ AlternativaSistemas.TesteTecnico/AlternativaSistemas.TesteTecnico.API/Services/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using AlternativaSistemas.TesteTecnico.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlternativaSistemas.TesteTecnico.API.Services {
+    public class DataContext : DbContext{
+        public DataContext(DbContextOptions<DataContext> options) : base(options){
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
